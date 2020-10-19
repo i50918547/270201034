5a08ce783f988814ca807d5c845cb946932d2cc4
i50918547 <robinvegoblin@gmail.com>
2020-10-19 09:28:33 +0000

    hello

---
+++ lab2/example1.py
@@ -0,0 +1,4 @@
+animal_number = 10
+chicken_number = 6
+rooster_number = animal_number - chicken_number
+print(rooster_number)
---
+++ lab2/example4.py
@@ -0,0 +1,3 @@
+celcius_degree = input("What is the temperature?")
+fahrenheit_degree = celcius_degree * 1.8 + 32
+print("fahrenheit_degree")
