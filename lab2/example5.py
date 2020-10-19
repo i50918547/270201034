8aaae1c1d199a87457706be81c22ee0e6ba71be7
i50918547 <robinvegoblin@gmail.com>
2020-10-19 10:08:31 +0000

    examples

---
+++ lab2/example5.py
@@ -0,0 +1,6 @@
+ab = int(input("Enter ab: "))
+bc = int(input("Enter bc: "))
+ac = (bc**2 + ab**2)**0.5
+print(ac)
+
+
---
+++ lab2/example6.py
@@ -0,0 +1,10 @@
+v1 = 80
+v2 = 70 
+distanceB = 490
+distanceE = 150
+
+minutes = 60 * (distanceB - distanceE) / (v1 + v2)
+
+print(minutes)
+
+
